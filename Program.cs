c66e8fcf2c937f9aff17af92e549856968687a48
melika reisi <126372164+melikareisi@users.noreply.github.com>
2023-06-25 17:51:00 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace ConsolApplication
+{
+    class program
+    {
+        public static void Main()
+
+        {
+            int[,] a = new int[50, 50];
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    Console.Write("Enter element [{0}, {1}]: ", i, j);
+                    a[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            calc(a);
+
+        }
+        private static void calc(int[,] a)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 48; i++)
+            {
+                for (int j = 1; j <= 48; j++)
+                {
+                    for (int n = i; n <= i + 2; n++)
+                    {
+                        for (int m = j; m <= j + 2; m++)
+                        {
+                            sum += sum + a[n, m];
+                        }
+                    }
+                    a[i, j] = sum / 9;
+                }
+            }
+            for (int i = 0; i <= 49; i++)
+            {
+                for (int j = 0; j <= 49; j++)
+                {
+                    Console.WriteLine(a[i, j]);
+                }
+            }
+        }
+    }
+}
